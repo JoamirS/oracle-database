ad1279cf701b16ba57801b02444faee7fc82a08e
jmoraes <jmoraes@localhost.localdomain>
2021-03-26 23:14:41 -0300

    inserting data

---
+++ Class_04.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_produtos
+(produto, nome, embalagem, tamanho, sabor, preco_lista)
+VALUES 
+('1040107', 'Light - 350 ml - Melancia', 'Lata', '350 ml', 'Melancia', 4.56);
+
+select * from tb_produtos;
