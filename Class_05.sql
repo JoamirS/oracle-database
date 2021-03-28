bc8c9f241b39d14ce3d1715222023fe0ac34e96c
jmoraes <jmoraes@localhost.localdomain>
2021-03-28 13:15:09 -0300

    Challenge insert into

--- Class_05.sql
+++ Class_05.sql
@@ -2,5 +2,5 @@ select * from tabela_de_vendedores;
 
 INSERT INTO tabela_de_vendedores (MATRICULA, NOME, PERCENTUAL_COMISSAO)
 VALUES 
-('00233', 'José Geraldo da Fonseca', 0.10)
+('00233', 'José Geraldo da Fonseca', 0.10);
 
