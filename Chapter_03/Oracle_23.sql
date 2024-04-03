d6d28607ea44e490a6c08f121f764442f0cb7be5
Joamir Silveira <jmrsilveira@outlook.com>
2024-04-03 00:58:01 -0300

    IN and LIKE function

--- Chapter_03/Oracle_23.sql
+++ Chapter_03/Oracle_23.sql
@@ -6,7 +6,7 @@ SELECT * FROM employees WHERE employee_name LIKE '%son%';
 
 SELECT * FROM employees WHERE department_id IN (101, 102, 103);
 
-SELECT * FROM employees WHERE department_id IN (SELECT department_id FROM departments WHERE location = 'New York');
+SELECT * FROM employees WHERE department_id IN (SELECT department_id FROM departments WHERE location = '%New York');
 
 DECLARE
     department_list VARCHAR2(100);
@@ -15,4 +15,9 @@ BEGIN
     EXECUTE IMMEDIATE 'SELECT * FROM employees WHERE department_id IN (' || department_list || ')';
 END;
 
-SELECT SHIP_NAME FROM SHIPS ORDER BY LIKE SHIP_NAME, CAPACITY DESC 
+SELECT SHIP_NAME FROM SHIPS ORDER BY SHIP_NAME DESC;
+
+SELECT * FROM SHIPS WHERE SHIP_NAME IN ('_Luís');
+-- The second row will be ignored because even thought the set of expressions within the IN clause includes a value for
+-- '_Luís' and uses the percent sign wildcard character at the beginning, the wildcard isn't activated because LIKE ins't
+-- present in the expression.
