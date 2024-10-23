88865eaf13bbb955c8453f5a34cd1c394a0b0728
Joamir Silveira <jmrsilveira@outlook.com>
2024-10-22 23:53:16 -0300

    update LPAD

--- Chapter_04/Oracle_27.sql
+++ Chapter_04/Oracle_27.sql
@@ -2,6 +2,8 @@
 
 SELECT RPAD('Chapter One - I Am Born', 30, '.') FROM DUAL;
 
+SELECT LPAD('Chapter Two - I am die', 30, '-') FROM DUAL;
+
 SELECT RPAD(CHAPTER_TITLE || ' ', 30, '.') || LPAD(' ' || PAGE_NUMBER, 30, ' ')
     "Table of Contents" FROM BOOK_CONTENTS ORDER BY PAGE_NUMBER;
     
@@ -9,4 +11,4 @@ SELECT RPAD(CHAPTER_TITLE || ' ', 30, '.') || LPAD(' ' || PAGE_NUMBER, 30, ' ')
 
 SELECT RTRIM('Seven trousand -------', '-') Result FROM DUAL;
 
-SELECT LTRIM('Seven trousand -------', '-') Result FROM DUAL;
+SELECT LTRIM('-- Seven trousand -------', '-') Result FROM DUAL;
